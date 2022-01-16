cd2b7f30a4fecb14a1d7585a94e2e7d14b5e6ea2
sacundim <anybody@sacundim.org>
2022-01-15 19:05:34 -0800

    Refactor in preparation for downloading other data sets

--- src/covid_19_puerto_rico_downloader/bioportal.py
+++ src/covid_19_puerto_rico_downloader/bioportal.py
@@ -9,16 +9,16 @@ import naya
 from pyarrow import json as arrow_json
 import pyarrow
 import pyarrow.parquet as parquet
-from pytz import timezone
 import requests
 import subprocess
 
 
 BIOPORTAL_URL = "https://bioportal.salud.pr.gov/api/administration/reports"
-DEATHS_ENDPOINT = f"{BIOPORTAL_URL}/deaths/summary"
-TESTS_ENDPOINT = f"{BIOPORTAL_URL}/minimal-info-unique-tests"
-ORDERS_ENDPOINT = f"{BIOPORTAL_URL}/orders/basic"
-
+ENDPOINTS = {
+    'deaths': f"{BIOPORTAL_URL}/deaths/summary",
+    'minimal-info-unique-tests': f"{BIOPORTAL_URL}/minimal-info-unique-tests",
+    'orders-basic': f"{BIOPORTAL_URL}/orders/basic"
+}
 
 def process_arguments():
     parser = argparse.ArgumentParser(description='Download Bioportal COVID-19 data sets')
@@ -32,14 +32,14 @@ def bioportal_download():
     args = process_arguments()
     now = datetime.now()
     logging.info('Now = %s', now.isoformat())
-    deaths_download(now, args)
+    download_dataset('deaths', now, args) # TODO: download other two
 
-def deaths_download(now, args):
-    json_path = make_filename('deaths', now, 'json')
-    jsonl_path = make_filename('deaths', now, 'jsonl')
-    parquet_path = make_filename('deaths', now, 'parquet')
+def download_dataset(name, now, args):
+    json_path = make_filename(name, now, 'json')
+    jsonl_path = make_filename(name, now, 'jsonl')
+    parquet_path = make_filename(name, now, 'parquet')
 
-    download_url(DEATHS_ENDPOINT, json_path)
+    download_url(ENDPOINTS[name], json_path)
     json2jsonl(json_path, jsonl_path, now)
     compress_file(json_path)
     table = arrow_json.read_json(jsonl_path)
@@ -53,7 +53,7 @@ def make_filename(basename, now, extension):
 
 def download_url(url, outpath):
     logging.info('Downloading %s from %s', outpath, url)
-    r = requests.get(url)
+    r = requests.get(url, headers={'Accept-encoding': 'gzip'})
     with open(outpath, 'wb') as fd:
         for chunk in r.iter_content(chunk_size=128):
             fd.write(chunk)
